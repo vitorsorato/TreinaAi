233a453684601765bfd71d0602cd29a1c5eb687e
vitor.fontana <vitorsorato@hotmail.com>
2025-06-15 15:01:02 -0300

    Initial Commit

---
+++ TreinaAi/TreinaAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreinaAiApp.swift
+//  TreinaAi
+//
+//  Created by Vitor Sorato on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TreinaAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TreinaAiTests/TreinaAiTests.swift
@@ -0,0 +1,16 @@
+//
+//  TreinaAiTests.swift
+//  TreinaAiTests
+//
+//  Created by Vitor Sorato on 15/06/25.
+//
+
+import Testing
+
+struct TreinaAiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
